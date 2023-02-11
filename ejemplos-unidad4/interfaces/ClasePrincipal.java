62bd004cba6a7bad8fee27b53056012c4bd496c0
Juan Pablo Chavez Arteaga <juanp26842@gmail.com>
2023-02-10 22:36:03 -0400

    (Repo clean up)Please do not commit IDE specific files and compiled files

--- ejemplos-unidad4/interfaces/ClasePrincipal.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-public class ClasePrincipal {
-    public static void main(String[] args) {
-        MiInterfaz clase = new ClaseImplementadora();
-        clase.metodo1();
-        clase.metodo2();
-    }
-}
--- ejemplos-unidad4/interfaces/MiInterfaz.java
+++
@@ -1,7 +0,0 @@
-package interfaces;
-
-public interface MiInterfaz {
-    int MI_CONSTANTE = 3; // public static final
-    void metodo1(); // public abstract
-    void metodo2();
-}
